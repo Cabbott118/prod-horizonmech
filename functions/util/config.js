f37e06a4ff6e14c689dc630963801803785fa240
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2021-04-14 08:33:39 -0400

    Files ready for upload to server

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  apiKey: 'AIzaSyATSVl5Llj57XfIZr8IP5ueeCuPRui1gKs',
-  authDomain: 'dev-horizonmech.firebaseapp.com',
-  databaseURL: 'https://dev-horizonmech-default-rtdb.firebaseio.com',
-  projectId: 'dev-horizonmech',
-  storageBucket: 'dev-horizonmech.appspot.com',
-  messagingSenderId: '1054637362181',
-  appId: '1:1054637362181:web:6cc781a6f70da0f3fae06c',
+  apiKey: 'AIzaSyCPPi4Dp90Defap0vln1OT3Mw6hYXeOAxQ',
+  authDomain: 'prod-horizonmech.firebaseapp.com',
+  projectId: 'prod-horizonmech',
+  storageBucket: 'prod-horizonmech.appspot.com',
+  messagingSenderId: '395964892404',
+  appId: '1:395964892404:web:c72991041bb690542b4aa1',
+  measurementId: 'G-XQ9B589Z2D',
 };
